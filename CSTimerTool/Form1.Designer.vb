ca728dc18ff3d1f80e00db731265e16707688bac
TyRmYz <aki.berg@luksia.fi>
2016-06-30 15:59:10 +0300

    Add files via upload

---
+++ CSTimerTool/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CSTimerTool
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(CSTimerTool))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.CSStatus = New System.Windows.Forms.Label()
+        Me.timerstatus = New System.Windows.Forms.Label()
+        Me.Traystuff = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'CSStatus
+        '
+        Me.CSStatus.AutoSize = True
+        Me.CSStatus.Location = New System.Drawing.Point(12, 275)
+        Me.CSStatus.Name = "CSStatus"
+        Me.CSStatus.Size = New System.Drawing.Size(73, 13)
+        Me.CSStatus.TabIndex = 0
+        Me.CSStatus.Text = "CSGO Status:"
+        '
+        'timerstatus
+        '
+        Me.timerstatus.AutoSize = True
+        Me.timerstatus.Location = New System.Drawing.Point(254, 275)
+        Me.timerstatus.Name = "timerstatus"
+        Me.timerstatus.Size = New System.Drawing.Size(69, 13)
+        Me.timerstatus.TabIndex = 1
+        Me.timerstatus.Text = "Timer Status:"
+        '
+        'Traystuff
+        '
+        Me.Traystuff.Text = "CS Timer Tool"
+        Me.Traystuff.Visible = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.CSTimerTool.My.Resources.Resources.smallcs1
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(482, 272)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'CSTimerTool
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(475, 293)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.timerstatus)
+        Me.Controls.Add(Me.CSStatus)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "CSTimerTool"
+        Me.Text = "CS Timer Tool by KURttU"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents CSStatus As System.Windows.Forms.Label
+    Friend WithEvents timerstatus As System.Windows.Forms.Label
+    Friend WithEvents Traystuff As System.Windows.Forms.NotifyIcon
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ CSTimerTool/Form1.vb
@@ -0,0 +1,94 @@
+﻿
+Imports System.Runtime.InteropServices
+Public Class CSTimerTool
+    <DllImport("ntdll.dll", EntryPoint:="NtSetTimerResolution")>
+    Public Shared Sub NtSetTimerResolution(desiredResolution As UInteger, setResolution As Boolean, ByRef currentResolution As UInteger)
+    End Sub
+    <DllImport("ntdll.dll", EntryPoint:="NtQueryTimerResolution")>
+    Public Shared Sub NtQueryTimerResolution(MinimumResoltuion As UInteger, MaximumResolution As UInteger, currentResolution As UInteger)
+    End Sub
+
+    Public ContextMenu1 As New ContextMenu
+
+    Private desiredResolution As UInteger = 5000
+    Private setResolution = True
+    Private currentResolution As UInteger
+    Private p() As Process
+    Private wasrunning As String
+    Private showrunning As String
+
+    Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.WindowState = FormWindowState.Minimized
+        Me.Visible = False
+
+        showrunning = "0"
+        CSStatus.Text = "CSGO Status: Not running"
+        timerstatus.Text = "Timer status: Not set"
+        Timer1.Enabled = True
+
+        Traystuff.Visible = True
+        Traystuff.Text = "CS Timer Tool"
+        Traystuff.Icon = My.Resources.cstt
+        Traystuff.BalloonTipIcon = ToolTipIcon.Info
+        Traystuff.BalloonTipTitle = "Timer tool starting"
+        Traystuff.BalloonTipText = "Application started and monitoring on background"
+        Traystuff.ShowBalloonTip(50000)
+
+        ShowInTaskbar = False
+
+        PictureBox1.Show()
+        PictureBox1.Refresh()
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        p = Process.GetProcessesByName("CSGO")
+        If p.Count > 0 Then
+            desiredResolution = 5000
+            NtSetTimerResolution(desiredResolution, setResolution, currentResolution)
+            timerstatus.Text = "Timer status: Set"
+            CSStatus.Text = "CSGO status: Running"
+            If showrunning = "0" Then
+                Traystuff.BalloonTipTitle = "CSGO Starting"
+                Traystuff.BalloonTipText = "Timer activated!"
+                Traystuff.ShowBalloonTip(1)
+                showrunning = "1"
+            Else
+
+            End If
+        Else
+            CSStatus.Text = "CSGO status: Not running"
+            timerstatus.Text = "Timer status: Not set"
+            If showrunning = "1" Then
+                Traystuff.BalloonTipTitle = "CSGO Closed"
+                Traystuff.BalloonTipText = "Timer Deactivated!"
+                Traystuff.ShowBalloonTip(1)
+                desiredResolution = 10000
+                NtSetTimerResolution(desiredResolution, setResolution, currentResolution)
+
+                showrunning = "0"
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub Traystuff_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Traystuff.DoubleClick
+
+        Me.WindowState = FormWindowState.Normal
+        Traystuff.Visible = True
+        ShowInTaskbar = False
+
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+
+        Me.WindowState = FormWindowState.Minimized
+        Me.Visible = False
+
+    End Sub
+
+End Class
